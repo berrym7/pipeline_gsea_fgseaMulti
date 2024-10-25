9de8152e3529d05e3a8ca4e5d36025c9c6518fb2
michaelberry <michaelberry@ochre-bio.com>
2024-10-25 14:29:28 +0000

    Edited run_gsea.sh

--- code/run_gsea.sh
+++ code/run_gsea.sh
@@ -5,17 +5,9 @@ source ./config.sh
 if [ -n "$DESEQ2" ] && [ -n "$PERTURBATION_ID" ] && [ -n "$OUTPUT_DIRECTORY" ];
 then
 
-    echo "DESEQ2 : $DESEQ2"
-    echo "Perturbation ID : $PERTURBATION_ID"
-    echo "Output directory : $OUTPUT_DIRECTORY"
-    echo "Gene Set : $GENE_SET"
-
     output=${OUTPUT_DIRECTORY}
 
-    Rscript fgsea.R \
-        --deg $DESEQ2 \
-        --output $output \
-        --gene_set $GENE_SET
+    echo "Command Being Run = Rscript fgsea.R --deg $DESEQ2 --output $output --gene_set $GENE_SET"
 
     else
     echo "This program requires something thats missing."
