ce2f942693aab205d426d1c682282997393fcd24
michaelberry <michaelberry@ochre-bio.com>
2024-10-28 10:05:52 +0000

    Edited run_gsea.sh

--- code/run_gsea.sh
+++ code/run_gsea.sh
@@ -8,7 +8,7 @@ then
     output=${OUTPUT_DIRECTORY}
 
     echo "Command Being Run = Rscript fgsea.R --deg $DESEQ2 --output $output --gene_set $GENE_SET"
-    Rscript fgsea.R --deg $DESEQ2 --output $output --gene_set $GENE_SET
+    #Rscript fgsea.R --deg $DESEQ2 --output $output --gene_set $GENE_SET
 
     else
     echo "This program requires something thats missing."
