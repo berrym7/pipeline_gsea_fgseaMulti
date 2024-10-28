cbab1b3ea8d3d2307b716991bad4e82d3f09ab37
michaelberry <michaelberry@ochre-bio.com>
2024-10-28 18:25:09 +0000

    Edited config.sh

--- code/config.sh
+++ code/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Inputs
-DESEQ2=$(find -L /data -name "DEG.tsv")
+DESEQ2=$(find -L /data -type f -name "DEG.tsv" -path "/data/ENSG*/*")
 PERTURBATION_ID=$(echo $DESEQ2 | cut -d"/" -f3)
 OUTPUT_DIRECTORY="/results/$PERTURBATION_ID/"
 GENE_SET=$(find -L /data/geneset -name "*.rds")
